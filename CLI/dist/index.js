9294f00d12c5daf350cd1692b199c2f86a64ce7c
Patafix <87929402+PatafixPLTX@users.noreply.github.com>
2024-01-27 16:40:52 +0000

    Create a CLI command to change value in the config.json with tempfolder command

---
+++ CLI/dist/index.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const { Command } = require("commander");
+const figlet = require("figlet");
+const fs_1 = __importDefault(require("fs"));
+const program = new Command();
+const configPath = "./../src/config.json";
+program
+    .version("1.0.0")
+    .description("TempFolderTool is a CLI tool to create a folder wich will delete files after a period of time")
+    .option("-p, --path  [value]", "change the path of the temp folder")
+    .option("-t, --time [value]", "change the time after which the files will be deleted")
+    .parse(process.argv);
+const options = program.opts();
+function changeTempFolderPath(filepath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const configData = fs_1.default.readFileSync(configPath, "utf-8");
+            const config = JSON.parse(configData);
+            config.tempFolderPath = filepath;
+            fs_1.default.writeFileSync(configPath, JSON.stringify(config, null, 2));
+        }
+        catch (error) {
+            console.error("Error occurred while reading or writing the config file!", error);
+        }
+    });
+}
+function changeRetentionPeriod(time) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const configData = fs_1.default.readFileSync(configPath, "utf-8");
+            const config = JSON.parse(configData);
+            config.retentionPeriod = time;
+            fs_1.default.writeFileSync(configPath, JSON.stringify(config, null, 2));
+        }
+        catch (error) {
+            console.error("Error occurred while reading or writing the config file!", error);
+        }
+    });
+}
+if (options.time) {
+    const time = typeof options.time === "string" ? options.time : "1";
+    changeRetentionPeriod(time);
+}
+if (options.path) {
+    const filepath = typeof options.path === "string" ? options.path : __dirname;
+    changeTempFolderPath(filepath);
+}
+if (!process.argv.slice(2).length) {
+    console.log(figlet.textSync("Temp Folder CLI"));
+    program.outputHelp();
+}
+//# sourceMappingURL=index.js.map
--- CLI/src/index.ts
+++ CLI/src/index.ts
@@ -1,5 +1,66 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
-const currentDateTime = new Date().toISOString()
+const { Command } = require("commander");
+const figlet = require("figlet");
+import fs from "fs";
 
-console.log(currentDateTime)
+const program = new Command();
+const configPath = "./../src/config.json"; // TODO change to can be used everywhere
+
+program
+    .version("0.0.1")
+    .description(
+        "TempFolderTool is a CLI tool to create a folder wich will delete files after a period of time"
+    )
+    .option("-p, --path  [value]", "change the path of the temp folder")
+    .option(
+        "-t, --time [value]",
+        "change the time after which the files will be deleted"
+    )
+    .parse(process.argv);
+
+const options = program.opts();
+
+async function changeTempFolderPath(filepath: string) {
+    try {
+        const configData = fs.readFileSync(configPath, "utf-8");
+        const config = JSON.parse(configData);
+        config.tempFolderPath = filepath;
+        fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+    } catch (error) {
+        console.error(
+            "Error occurred while reading or writing the config file!",
+            error
+        );
+    }
+}
+
+async function changeRetentionPeriod(time: string) {
+    try {
+        const configData = fs.readFileSync(configPath, "utf-8");
+        const config = JSON.parse(configData);
+        config.retentionPeriod = time;
+        fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+    } catch (error) {
+        console.error(
+            "Error occurred while reading or writing the config file!",
+            error
+        );
+    }
+}
+
+if (options.time) {
+    const time = typeof options.time === "string" ? options.time : "1";
+    changeRetentionPeriod(time);
+}
+
+if (options.path) {
+    const filepath =
+        typeof options.path === "string" ? options.path : __dirname;
+    changeTempFolderPath(filepath);
+}
+
+if (!process.argv.slice(2).length) {
+    console.log(figlet.textSync("Temp Folder CLI"));
+    program.outputHelp();
+}
