d14188ac048c1d7f7834a2aeb54d833971644e9b
Kamil LEYS <leys.kamil@gmail.com>
2024-02-01 17:49:39 +0100

    Basic working version(panics on windows but should be fixed in next update)

--- CLI-Bun/index.ts
+++ CLI-Bun/index.ts
@@ -4,10 +4,13 @@ import path from "node:path";
 const __dirname = import.meta.dir;
 const configPath = path.join(__dirname, "./../src/config.json");
 import { version } from "./package.json";
+import { BunFile } from "bun";
+import { $ } from "bun";
 
 register(["-c", "--config"], "show the current config", showConfig);
 register(["-v", "--version"], "show the cli version number", () => console.log(version));
-// register(["-p", "--path"], "change the path of the temp folder");
+register(["-p", "--path"], "change the path of the temp folder", changePath);
+register(["-g", "--github"], "open the github page", async () => await $`xdg-open http://google.com`);
 init();
 
 function showConfig() {
@@ -22,4 +25,18 @@ function showConfig() {
     } catch (error) {
         console.error("Error occurred while reading the config file!", error);
     }
+}
+
+async function changePath(args: string[]) {
+    const configFile: BunFile = Bun.file(configPath);
+
+    let { tempFolderPath, ...data } = await configFile.json();
+    tempFolderPath = args[0];
+    Bun.write(
+        configFile,
+        JSON.stringify({
+            ...data,
+            tempFolderPath,
+        })
+    )
 }
