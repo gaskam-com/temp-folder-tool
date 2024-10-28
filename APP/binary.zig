ba5f5071e834397b516e6a5c968901fd5654a28d
kamil-leys <72702161+kamil-leys@users.noreply.github.com>
2024-10-28 14:09:32 +0000

    small update

--- APP/binary.zig
+++ APP/binary.zig
@@ -1,29 +1,31 @@
 const std = @import("std");
-var GPA = std.heap.GeneralPurposeAllocator(.{}){};
-const allocator = GPA.allocator();
+const gzip = std.compress.gzip;
 
-const StartCharacter = 0b1111111;
-const EndCharacter = 0b1111110;
+const testString = 
+    \\{
+    \\    "D:\\Documents\\Github\\Gaskam\\temp-folder-tool\\APP\\Temp Folder": {
+    \\        "abc": 123,
+    \\         "def": 456,
+    \\         "ghi": 789
+    \\    }
+    \\}
+;
 
-const characters = "abcdefghijklmnopqrstuvwxyz123456789àáâãäåæçèéêëìíîïðñòóôõöøœùúûüýÞþß!#$%&'()+,-.;=@[]^`{}~ -/";
-const bytes = 7;
-
-const ToEncode = "/qmljfae qsdfmljaf-lahfecatcatcatcatcatcatcat.jpg";
 
 pub fn main() !void {
-    var output = std.ArrayList(u8).init(allocator);
-    defer output.deinit();
-    var spaceLeft: u3 = 0;
-    for (ToEncode) |character| {
-        if (spaceLeft == 0) {
-            try output.append(@as(u8, @truncate(std.mem.indexOf(u8, characters, &[1]u8{character}) orelse 0)));
-        } else {
-            std.debug.print("{d}\n", .{character});
-            const value = @as(u8, @truncate(std.mem.indexOf(u8, characters, &[1]u8{character}) orelse 0));
-            try output.append(output.pop() | @shlWithOverflow(value, @as(u3, @truncate(8 - @as(u4, spaceLeft))))[0]);
-            try output.append(value >> spaceLeft);
-        }
-        spaceLeft = 8 - bytes +% spaceLeft;
-    }
-    std.debug.print("{s}", .{output.items});
+    std.debug.print("ToEncode: {s}\n", .{testString});
+    // var GPA = std.heap.GeneralPurposeAllocator(.{}){};
+    // const allocator = GPA.allocator();
+
+    // var buffer = std.ArrayList(u8).init(allocator);
+    // defer buffer.deinit();
+    var file = try std.fs.cwd().openFile("customRetentions.bin", .{
+        .mode = .write_only,
+    });
+
+    var FBS = std.io.fixedBufferStream(testString);
+
+    try gzip.compress(FBS.reader(), file.writer(), .{});
+
+    // std.debug.print("Encoded: {s}\n", .{buffer.items});
 }
